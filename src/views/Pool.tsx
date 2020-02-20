10f2978fa4247cd50fc4bb366bc7530f7d1620a4
John Kelleher <johnpkelleher@gmail.com>
2020-02-19 23:30:34 -0500

    InfoPanel component

--- src/views/Pool.tsx
+++ src/views/Pool.tsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import ChartPanel   from '../components/Pool/ChartPanel';
 import AddRemovePanel   from '../components/Pool/AddRemovePanel';
+import InfoPanel from '../components/Pool/InfoPanel';
 
 const PoolViewWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     padding: 27px 25px 0px 25px;
 `
 
@@ -11,9 +15,10 @@ const InfoPanelWrapper = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-`
-
-const InfoPanel = styled.div`
+    width: 100%;
+    justify-content: flex-start;
+    div {
+    }
 `
 
 const BalancesTable = styled.div`
@@ -26,13 +31,13 @@ const Pool = () => {
 
     return (
         <PoolViewWrapper>
+        	<ChartPanel />
+        	<AddRemovePanel />
             <InfoPanelWrapper>
-            	<ChartPanel />
-            	<AddRemovePanel />
-            	<InfoPanel />
-            	<InfoPanel />
-            	<InfoPanel />
-            	<InfoPanel />
+            	<InfoPanel text="$ 8,024,093.89" subText="Liquidity" />
+                <InfoPanel text="$ 1,252,425.65" subText="Trade Volume (24hr)" />
+                <InfoPanel text="0.1%" subText="Pool Swap Fee" />
+                <InfoPanel text="10.00%" subText="My Pool Share" />
             </InfoPanelWrapper>
             <BalancesTable />
             <SwapsTable />
