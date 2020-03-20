95309c0146e340e6c5e2fef2b668ab198f1a5e82
tspoff@gmail.com <tspoff@gmail.com>
2020-03-20 12:28:42 -0400

    Increase decimal precision for correct percentage calcuations

--- src/utils/bignumber.ts
+++ src/utils/bignumber.ts
@@ -3,7 +3,7 @@ import { BigNumber } from 'bignumber.js';
 BigNumber.config({
     EXPONENTIAL_AT: [-100, 100],
     ROUNDING_MODE: BigNumber.ROUND_HALF_EVEN,
-    DECIMAL_PLACES: 18,
+    DECIMAL_PLACES: 40,
 });
 
 export { BigNumber };
