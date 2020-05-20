89a7ff20ea0f478bf7af778a6adc95dd6f03ee8e
Timur Badretdinov <destinerx@gmail.com>
2020-05-20 16:48:52 +0300

    Show pool creation form on NewPool page

--- src/components/Home/NewPool.tsx
+++ src/components/Home/NewPool.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { observer } from 'mobx-react';
+import { bnum, toWei } from 'utils/helpers';
+import { ContractTypes } from '../../stores/Provider';
+import { useStores } from '../../contexts/storesContext';
+import CreatePoolTable from '../CreatePool/CreatePoolTable';
+import Button from '../Common/Button';
 
 const Wrapper = styled.div`
     padding-top: 8px;
@@ -16,10 +21,134 @@ const Header = styled.div`
     padding: 0px 0px 24px 0px;
 `;
 
+const SingleElement = styled.div`
+    display: flex;
+`;
+
+const Section = styled.div`
+    margin-top: 16px;
+`;
+
+const InputWrapper = styled.div`
+    height: 30px;
+    padding: 0px 17px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid var(--panel-border);
+    background-color: var(--panel-background);
+    border-radius: 4px;
+    input {
+        width: 70px;
+        text-align: right;
+        color: var(--input-text);
+        font-size: 14px;
+        font-weight: 500;
+        line-height: 16px;
+        letter-spacing: 0.2px;
+        padding-left: 5px;
+        background-color: var(--panel-background);
+        border: none;
+        box-shadow: inset 0 0 0 1px var(--panel-background),
+            inset 0 0 0 70px var(--panel-background);
+        :-webkit-autofill,
+        :-webkit-autofill:hover,
+        :-webkit-autofill:focus,
+        :-webkit-autofill:active,
+        :-internal-autofill-selected {
+            -webkit-text-fill-color: var(--body-text);
+        }
+        ::placeholder {
+            color: var(--input-placeholder-text);
+        }
+        :focus {
+            outline: none;
+        }
+    }
+    border: ${props =>
+        props.errorBorders ? '1px solid var(--error-color)' : ''};
+    margin-left: ${props => (props.errorBorders ? '-1px' : '0px')}
+    margin-right: ${props => (props.errorBorders ? '-1px' : '0px')}
+    :hover {
+        background-color: var(--input-hover-background);
+        border: ${props =>
+            props.errorBorders
+                ? '1px solid var(--error-color)'
+                : '1px solid var(--input-hover-border);'};
+        margin-left: -1px;
+        margin-right: -1px;
+        input {
+            background-color: var(--input-hover-background);
+            box-shadow: inset 0 0 0 1px var(--input-hover-background),
+                inset 0 0 0 70px var(--input-hover-background);
+            ::placeholder {
+                color: var(--input-hover-placeholder-text);
+                background-color: var(--input-hover-background);
+            }
+        }
+    }
+`;
+
 const NewPool = observer(() => {
+    const {
+        root: {
+            contractMetadataStore,
+            createPoolFormStore,
+            providerStore,
+            tokenStore,
+        },
+    } = useStores();
+
+    const feeInput = createPoolFormStore.fee;
+    let hasError = false;
+
+    const handleAddButtonClick = async () => {
+        const trackedTokenAddresses = contractMetadataStore.getTrackedTokenAddresses();
+        const tokens = createPoolFormStore.tokens;
+        const newToken = trackedTokenAddresses.find(token => {
+            const isEther = token === 'ether';
+            const alreadyExists = tokens.includes(token);
+            return !isEther && !alreadyExists;
+        });
+        createPoolFormStore.addToken(newToken);
+    };
+
+    const handleInputChange = async event => {
+        const { value } = event.target;
+        createPoolFormStore.setFee(value);
+    };
+
     return (
         <Wrapper>
             <Header>Tokens</Header>
+            <CreatePoolTable />
+            <Section>
+                <SingleElement>
+                    <Button
+                        buttonText={'Add Token'}
+                        active={true}
+                        onClick={e => handleAddButtonClick()}
+                    />
+                </SingleElement>
+            </Section>
+            <Section>
+                <Header>Swap fee</Header>
+                <SingleElement>
+                    <InputWrapper errorBorders={hasError}>
+                        <input
+                            value={feeInput.value}
+                            onChange={e => {
+                                handleInputChange(e);
+                            }}
+                            placeholder=""
+                        />
+                    </InputWrapper>
+                </SingleElement>
+            </Section>
         </Wrapper>
     );
 });
