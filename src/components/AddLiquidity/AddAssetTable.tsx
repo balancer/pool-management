6ea749d8178809d51fc29a8eed90f03eed6267fb
Timur Badretdinov <destinerx@gmail.com>
2020-06-17 17:33:45 +0300

    Fix allowance checkbox in AddAssetTable

--- src/components/AddLiquidity/AddAssetTable.tsx
+++ src/components/AddLiquidity/AddAssetTable.tsx
@@ -341,7 +341,7 @@ const AddAssetTable = observer((props: Props) => {
                         hasMaxApproval = tokenStore.hasMaxApproval(
                             tokenAddress,
                             account,
-                            pool.address
+                            proxyAddress
                         );
                     }
 
