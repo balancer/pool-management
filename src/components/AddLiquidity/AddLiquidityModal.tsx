c3988fce4487ae41f36fea6ad3d473a781b0afdd
Timur Badretdinov <destinerx@gmail.com>
2020-07-06 13:35:13 +0300

    Clarify warning text

--- src/components/AddLiquidity/AddLiquidityModal.tsx
+++ src/components/AddLiquidity/AddLiquidityModal.tsx
@@ -629,9 +629,9 @@ const AddLiquidityModal = observer((props: Props) => {
             <Warning>
                 <Icon src="WarningSign.svg" />
                 <Content>
-                    Add liquidity might fail due to using max balance and a
-                    trade occuring before join tx is mined. Use high gas price
-                    or
+                    Adding liquidity might fail due to using max balance and a
+                    trade occuring before transaction is mined. Use high gas
+                    price or
                     <LowerAmountLink onClick={e => handleLowerAmountButton()}>
                         lower amounts
                     </LowerAmountLink>
