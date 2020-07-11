0a760389dccbf9326f985299faf2ada2c968cd85
Fabien <bonustrack@users.noreply.github.com>
2020-07-11 21:15:53 +0700

    Remove unused import
    Co-authored-by: Timur Badretdinov <destinerx@gmail.com>

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { RouteComponentProps } from 'react-router';
 import styled from 'styled-components';
 import SelectAssetModal from './SelectAssetModal';
 import { useStores } from '../../contexts/storesContext';
