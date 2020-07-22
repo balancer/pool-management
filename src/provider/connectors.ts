96e1d1b97f51c011ea02e09f2ff9bc45a2f31f82
Mike McDonald <mikeraymcdonald@gmail.com>
2020-07-21 20:24:58 -0500

    update dai usdc token addresses

--- src/provider/connectors.ts
+++ src/provider/connectors.ts
@@ -54,12 +54,14 @@ export const isChainIdSupported = (chainId: number): boolean => {
 const RPC_URLS: { [chainId: number]: string } = {
     1: process.env.REACT_APP_RPC_URL_1 as string,
     3: process.env.REACT_APP_RPC_URL_3 as string,
+    4: process.env.REACT_APP_RPC_URL_4 as string,
     42: process.env.REACT_APP_RPC_URL_42 as string,
 };
 
 export const SUBGRAPH_URLS: { [chainId: number]: string } = {
     1: process.env.REACT_APP_SUBGRAPH_URL_1 as string,
     3: process.env.REACT_APP_SUBGRAPH_URL_3 as string,
+    4: process.env.REACT_APP_SUBGRAPH_URL_4 as string,
     42: process.env.REACT_APP_SUBGRAPH_URL_42 as string,
 };
 
